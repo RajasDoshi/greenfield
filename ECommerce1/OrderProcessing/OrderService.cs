b8e3e9fea0a6eeccc3ff50a705f9c4cf17912363
RajasDoshi <87440634+RajasDoshi@users.noreply.github.com>
2024-11-08 22:59:17 +0530

    OrderService.cs

--- ECommerce1/OrderProcessing/OrderService.cs
+++ ECommerce1/OrderProcessing/OrderService.cs
@@ -1,12 +1,60 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OrderProcessing
 {
-    internal class OrderService
+    public class OrderService : IOrderService
     {
+        private List<Order> _orders;
+        public OrderService()
+        {
+            this._orders = new List<Order>();
+        }
+        public bool Delete(int id)
+        {
+            Order theOrder = this._orders.Find(o => o.Id == id);
+            Debug.WriteLine(theOrder.Amount);
+            
+            _orders.Remove(theOrder);
+            return true;
+        }
+
+        public List<Order> GetAllOrders()
+        {
+             return _orders;
+        }
+
+        public Order GetOrder(int id)
+        {
+            foreach (Order theOrder in _orders)
+            {
+                if(theOrder.Id == id) return theOrder;
+            }
+            return null;
+        }
+
+        public bool Insert(Order order)
+        {
+            this._orders.Add(order);
+            return true;
+        }
+
+        public bool Update(Order order)
+        {
+            this.Delete(order.Id);
+            this.Insert(order);
+            return true;
+        }
+
+        public List<Order> GetOrdersByDate(DateTime date)
+        {
+            // Filter orders where the OrderDate matches the specified date
+            return _orders.FindAll(order => order.Date == date.Date).ToList();
+        }
+
     }
 }
