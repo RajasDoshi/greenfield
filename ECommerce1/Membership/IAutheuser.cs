e58339b621b833a30943afe01d0a0287fd4715b7
RajasDoshi <87440634+RajasDoshi@users.noreply.github.com>
2024-11-08 22:48:39 +0530

    IAutheuser.cs

--- ECommerce1/Membership/IAutheuser.cs
+++ ECommerce1/Membership/IAutheuser.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Membership
 {
-    internal class IAutheuser
+    internal interface IAuthService
     {
+        bool Login(string username, string password);
+        bool Register(string username, string password);
+        bool ForgotPassword(string username);
+        bool ResetPassword(string username, string oldpassword, string newpassword);
     }
 }
